23d8b4b3cbcfe9c67ba2ae5b1e88631e3264657b
gabrieeelsp <gabrieeelsp@gmil.com>
2020-05-03 18:47:31 -0300

    version 2.1

--- database/migrations/2020_05_03_120359_create_rubros_table.php
+++ database/migrations/2020_05_03_120359_create_rubros_table.php
@@ -14,7 +14,7 @@ class CreateRubrosTable extends Migration
     public function up()
     {
         Schema::create('rubros', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name', 128);
             $table->string('slug', 128)->unique();
             //$table->timestamps();
